c1330c1484d0fcf57a101fa2d6df7edb8305b6a5
shenzhenyu <876619304@qq.com>
2022-04-08 16:56:29 +0800

    提交

--- src/main/java/com/example/firstdemo/FirstdemoApplication.java
+++ src/main/java/com/example/firstdemo/FirstdemoApplication.java
@@ -2,14 +2,10 @@ package com.example.firstdemo;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //注释一下
